4409a526bf771a453dc7014a1fde5b08673e04ed
Serg-zefir <serg.takij@gmail.com>
2016-12-01 17:49:22 +0200

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          
+          <h2>Welcome to my React app</h2>
+        </div>
+        <p className="App-intro">
+          save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
